4ce340131664d1ca10f7b89f8ae142efd811b04e
Samuel Chua <chua.jiacong.samuel@dhs.sg>
2022-03-07 14:58:06 +0800

    Added custom function to toggle recording using on/off push button, produced binary IQ raw data but timestamp doesn't work

---
+++ extract.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Extract
+# GNU Radio version: v3.8.2.0-57-gd71cd177
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import eng_notation
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import blocks
+from gnuradio import gr
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import uhd
+import time
+from gnuradio.qtgui import Range, RangeWidget
+
+from gnuradio import qtgui
+
+class extract(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Extract")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Extract")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "extract")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.variable_qtgui_label_1 = variable_qtgui_label_1 = 0
+        self.variable_qtgui_label_0 = variable_qtgui_label_0 = 0
+        self.tuning = tuning = 98.2e6
+        self.samp_rate = samp_rate = 32000
+        self.record = record = 1
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._tuning_range = Range(70e6, 3000e6, 200e4, 98.2e6, 200)
+        self._tuning_win = RangeWidget(self._tuning_range, self.set_tuning, 'tuning', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._tuning_win)
+        self._variable_qtgui_label_1_tool_bar = Qt.QToolBar(self)
+
+        if None:
+            self._variable_qtgui_label_1_formatter = None
+        else:
+            self._variable_qtgui_label_1_formatter = lambda x: str(x)
+
+        self._variable_qtgui_label_1_tool_bar.addWidget(Qt.QLabel('End Record' + ": "))
+        self._variable_qtgui_label_1_label = Qt.QLabel(str(self._variable_qtgui_label_1_formatter(self.variable_qtgui_label_1)))
+        self._variable_qtgui_label_1_tool_bar.addWidget(self._variable_qtgui_label_1_label)
+        self.top_grid_layout.addWidget(self._variable_qtgui_label_1_tool_bar)
+        self._variable_qtgui_label_0_tool_bar = Qt.QToolBar(self)
+
+        if None:
+            self._variable_qtgui_label_0_formatter = None
+        else:
+            self._variable_qtgui_label_0_formatter = lambda x: str(x)
+
+        self._variable_qtgui_label_0_tool_bar.addWidget(Qt.QLabel('Start Record' + ": "))
+        self._variable_qtgui_label_0_label = Qt.QLabel(str(self._variable_qtgui_label_0_formatter(self.variable_qtgui_label_0)))
+        self._variable_qtgui_label_0_tool_bar.addWidget(self._variable_qtgui_label_0_label)
+        self.top_grid_layout.addWidget(self._variable_qtgui_label_0_tool_bar)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+            ",".join(("", "")),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+        )
+        self.uhd_usrp_source_0.set_center_freq(tuning, 0)
+        self.uhd_usrp_source_0.set_gain(50, 0)
+        self.uhd_usrp_source_0.set_antenna('TX/RX', 0)
+        self.uhd_usrp_source_0.set_bandwidth(samp_rate, 0)
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec())
+        _record_push_button = Qt.QPushButton('record')
+        _record_push_button = Qt.QPushButton('record')
+        self._record_choices = {'Pressed': 1, 'Released': 0}
+        _record_push_button.pressed.connect(lambda: self.set_record(self._record_choices['Pressed']))
+        _record_push_button.released.connect(lambda: self.set_record(self._record_choices['Released']))
+        self.top_grid_layout.addWidget(_record_push_button)
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            1024, #fftsize
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(True)
+
+        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\intern\\Documents\\GitHub\\Wifi-Cam\\signal_data\\filename', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.recording_state = False
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_sink_x_0, 0))
+
+
+    def toggle_recording(self):
+        timestamp = time.localtime() #Use localtime for generating filenames
+        filename = "%04d_%02d_%02d_%02d:%02d:%02d.dat" % (timestamp.tm_year, timestamp.tm_mon, timestamp.tm_mday, timestamp.tm_hour, timestamp.tm_min, timestamp.tm_sec)
+        if (self.recording_state == True):
+            self.recording_state = False
+            self._record_choices = 0
+            print ("Recording Data: Off")
+            self.blocks_file_sink_0.close()
+        else:
+            self.recording_state = True
+            print ("Recording Data to" + filename)
+            self._record_choices = 1
+            self.blocks_file_sink_0.open(self.prefix+filename)    
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "extract")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_variable_qtgui_label_1(self):
+        return self.variable_qtgui_label_1
+
+    def set_variable_qtgui_label_1(self, variable_qtgui_label_1):
+        self.variable_qtgui_label_1 = variable_qtgui_label_1
+        Qt.QMetaObject.invokeMethod(self._variable_qtgui_label_1_label, "setText", Qt.Q_ARG("QString", self.variable_qtgui_label_1))
+
+    def get_variable_qtgui_label_0(self):
+        return self.variable_qtgui_label_0
+
+    def set_variable_qtgui_label_0(self, variable_qtgui_label_0):
+        self.variable_qtgui_label_0 = variable_qtgui_label_0
+        Qt.QMetaObject.invokeMethod(self._variable_qtgui_label_0_label, "setText", Qt.Q_ARG("QString", self.variable_qtgui_label_0))
+
+    def get_tuning(self):
+        return self.tuning
+
+    def set_tuning(self, tuning):
+        self.tuning = tuning
+        self.uhd_usrp_source_0.set_center_freq(self.tuning, 0)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_bandwidth(self.samp_rate, 0)
+
+    def get_record(self):
+        return self.record
+
+    def set_record(self, record):
+        self.record = record
+
+
+
+
+
+def main(top_block_cls=extract, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
