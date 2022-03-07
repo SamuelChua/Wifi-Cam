348db0cfa4a9f524e8c02176de5e129879842470
Samuel Chua <chua.jiacong.samuel@dhs.sg>
2022-03-07 17:15:15 +0800

    QWidget: Must construct a QApplication before a QWidget

--- trigger.py
+++ trigger.py
@@ -1,25 +1,30 @@
-import stream
+import tagged_file_sink_test
 import time
+import PyQt5
 
-print ("Creating stream class")
-tb = stream.stream()
+def runApp():
+	newApp = PyQt5.QtWidgets.QApplication(sys.argv)
+	print ("Creating tagged_file_sink_test class")
+	tb = tagged_file_sink_test.tagged_file_sink_test()
 
-print ("Starting stream...")
-tb.start()
-print ("Started...")
+	print ("Starting tagged_file_sink_test...")
+	tb.start()
+	print ("Started...")
 
-#Testing the trigger
+	#Testing the trigger
 
-print ("Saving...")
-tb.set_trigger(1)
-time.sleep(2)
-tb.set_trigger(-1)
-print("Stopping...")
+	print ("Saving...")		
+	tb.set_trigger(1)
+	time.sleep(2)
+	tb.set_trigger(-1)
+	print("Stopping...")
 
-time.sleep(2)
+	time.sleep(2)
 
-print ("Saving...")
-tb.set_trigger(1)
-time.sleep(2)
-tb.set_trigger(-1)
-print("Stopping...")
+	print ("Saving...")
+	tb.set_trigger(1)
+	time.sleep(2)
+	tb.set_trigger(-1)
+	print("Stopping...")
+
+runApp()
