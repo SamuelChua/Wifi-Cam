63b16f96c952b4cf43a7723808334e62f2b832f9
Samuel Chua <chua.jiacong.samuel@dhs.sg>
2022-03-18 11:09:29 +0800

    Added timed intervals to mute datastream into file sink + Manual on/off of USRP using checkbox

--- extract.py
+++ extract.py
@@ -21,20 +21,23 @@ if __name__ == '__main__':
             print("Warning: failed to XInitThreads()")
 
 from PyQt5 import Qt
-from gnuradio import eng_notation
+from PyQt5.QtCore import QObject, pyqtSlot
 from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
+from datetime import datetime
+from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import gr
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
 from gnuradio import uhd
 import time
 from gnuradio.qtgui import Range, RangeWidget
-from datetime import datetime
+
 from gnuradio import qtgui
 
 class extract(gr.top_block, Qt.QWidget):
@@ -73,40 +76,35 @@ class extract(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.variable_qtgui_label_1 = variable_qtgui_label_1 = 0
-        self.variable_qtgui_label_0 = variable_qtgui_label_0 = 0
-        self.tuning = tuning = 98.2e6
-        self.samp_rate = samp_rate = 32000
-        self.record = record = 1
+        self.prefix = prefix = 'C:\\Users\\intern\\Documents\\GitHub\\Wifi-Cam\\signal_data\\filename'
+        self.tuning = tuning = 2.422e9
+        self.time = time = prefix + datetime.now().strftime("%H.%M.%S")
+        self.samp_rate = samp_rate = 1e6
+        self.duration = duration = 1
+        self.Switch = Switch = 0
 
         ##################################################
         # Blocks
         ##################################################
-        self._tuning_range = Range(70e6, 3000e6, 200e4, 98.2e6, 200)
+        self._tuning_range = Range(70e6, 3e9, 2e6, 2.422e9, 200)
         self._tuning_win = RangeWidget(self._tuning_range, self.set_tuning, 'tuning', "counter_slider", float)
         self.top_grid_layout.addWidget(self._tuning_win)
-        self._variable_qtgui_label_1_tool_bar = Qt.QToolBar(self)
-
-        if None:
-            self._variable_qtgui_label_1_formatter = None
-        else:
-            self._variable_qtgui_label_1_formatter = lambda x: str(x)
-
-        self._variable_qtgui_label_1_tool_bar.addWidget(Qt.QLabel('End Record' + ": "))
-        self._variable_qtgui_label_1_label = Qt.QLabel(str(self._variable_qtgui_label_1_formatter(self.variable_qtgui_label_1)))
-        self._variable_qtgui_label_1_tool_bar.addWidget(self._variable_qtgui_label_1_label)
-        self.top_grid_layout.addWidget(self._variable_qtgui_label_1_tool_bar)
-        self._variable_qtgui_label_0_tool_bar = Qt.QToolBar(self)
-
-        if None:
-            self._variable_qtgui_label_0_formatter = None
-        else:
-            self._variable_qtgui_label_0_formatter = lambda x: str(x)
-
-        self._variable_qtgui_label_0_tool_bar.addWidget(Qt.QLabel('Start Record' + ": "))
-        self._variable_qtgui_label_0_label = Qt.QLabel(str(self._variable_qtgui_label_0_formatter(self.variable_qtgui_label_0)))
-        self._variable_qtgui_label_0_tool_bar.addWidget(self._variable_qtgui_label_0_label)
-        self.top_grid_layout.addWidget(self._variable_qtgui_label_0_tool_bar)
+        # Create the options list
+        self._Switch_options = (0, 1, )
+        # Create the labels list
+        self._Switch_labels = ('Off', 'On', )
+        # Create the combo box
+        self._Switch_tool_bar = Qt.QToolBar(self)
+        self._Switch_tool_bar.addWidget(Qt.QLabel('Switch' + ": "))
+        self._Switch_combo_box = Qt.QComboBox()
+        self._Switch_tool_bar.addWidget(self._Switch_combo_box)
+        for _label in self._Switch_labels: self._Switch_combo_box.addItem(_label)
+        self._Switch_callback = lambda i: Qt.QMetaObject.invokeMethod(self._Switch_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._Switch_options.index(i)))
+        self._Switch_callback(self.Switch)
+        self._Switch_combo_box.currentIndexChanged.connect(
+            lambda i: self.set_Switch(self._Switch_options[i]))
+        # Create the radio buttons
+        self.top_grid_layout.addWidget(self._Switch_tool_bar)
         self.uhd_usrp_source_0 = uhd.usrp_source(
             ",".join(("", "")),
             uhd.stream_args(
@@ -121,16 +119,10 @@ class extract(gr.top_block, Qt.QWidget):
         self.uhd_usrp_source_0.set_bandwidth(samp_rate, 0)
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec())
-        _record_push_button = Qt.QPushButton('record')
-        _record_push_button = Qt.QPushButton('record')
-        self._record_choices = {'Pressed': 1, 'Released': 0}
-        _record_push_button.pressed.connect(lambda: self.set_record(self._record_choices['Pressed']))
-        _record_push_button.released.connect(lambda: self.set_record(self._record_choices['Released']))
-        self.top_grid_layout.addWidget(_record_push_button)
         self.qtgui_sink_x_0 = qtgui.sink_c(
             1024, #fftsize
             firdes.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
+            tuning, #fc
             samp_rate, #bw
             "", #name
             True, #plotfreq
@@ -144,73 +136,72 @@ class extract(gr.top_block, Qt.QWidget):
         self.qtgui_sink_x_0.enable_rf_freq(True)
 
         self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\intern\\Documents\\GitHub\\Wifi-Cam\\signal_data\\filename' + datetime.now().strftime("%Y.%m.%d.%H.%M.%S"), False)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(Switch)
+        self.blocks_message_debug_0 = blocks.message_debug()
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, time, False)
         self.blocks_file_sink_0.set_unbuffered(False)
-        self.recording_state = False
+        self.analog_pwr_squelch_xx_0 = analog.pwr_squelch_cc(-140, 1e-4, 0, True)
+
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.analog_pwr_squelch_xx_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.analog_pwr_squelch_xx_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.analog_pwr_squelch_xx_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_const_vxx_0, 0))
 
-
-
-    def toggle_recording(self): #Using pushbutton GUI
-        #timestamp = time.localtime() #Use localtime for generating filenames
-        #filename = "%04d_%02d_%02d_%02d:%02d:%02d.dat" % (timestamp.tm_year, timestamp.tm_mon, timestamp.tm_mday, timestamp.tm_hour, timestamp.tm_min, timestamp.tm_sec)
-        if (self.record== 1):
-            self.record = 0
-            self._record_choices = 0
-            print ("Recording Data: Off")
-            self.blocks_file_sink_0.close()
-        else:
-            self.record = 1
-            print ("Recording Data to" + filename)
-            self._record_choices = 1
-            self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\intern\\Documents\\GitHub\\Wifi-Cam\\signal_data\\filename' + datetime.now().strftime("%Y.%m.%d.%H.%M.%S"), False)  
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "extract")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
-    def get_variable_qtgui_label_1(self):
-        return self.variable_qtgui_label_1
+    def get_prefix(self):
+        return self.prefix
 
-    def set_variable_qtgui_label_1(self, variable_qtgui_label_1):
-        self.variable_qtgui_label_1 = variable_qtgui_label_1
-        Qt.QMetaObject.invokeMethod(self._variable_qtgui_label_1_label, "setText", Qt.Q_ARG("QString", self.variable_qtgui_label_1))
-
-    def get_variable_qtgui_label_0(self):
-        return self.variable_qtgui_label_0
-
-    def set_variable_qtgui_label_0(self, variable_qtgui_label_0):
-        self.variable_qtgui_label_0 = variable_qtgui_label_0
-        Qt.QMetaObject.invokeMethod(self._variable_qtgui_label_0_label, "setText", Qt.Q_ARG("QString", self.variable_qtgui_label_0))
+    def set_prefix(self, prefix):
+        self.prefix = prefix
+        self.set_time(self.prefix + datetime.now().strftime("%H.%M.%S"))
 
     def get_tuning(self):
         return self.tuning
 
     def set_tuning(self, tuning):
         self.tuning = tuning
+        self.qtgui_sink_x_0.set_frequency_range(self.tuning, self.samp_rate)
         self.uhd_usrp_source_0.set_center_freq(self.tuning, 0)
+
+    def get_time(self):
+        return self.time
+
+    def set_time(self, time):
+        self.time = time
+        self.blocks_file_sink_0.open(self.time)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(self.tuning, self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_source_0.set_bandwidth(self.samp_rate, 0)
 
-    def get_record(self):
-        return self.record
+    def get_duration(self):
+        return self.duration
 
-    def set_record(self, record):
-        self.record = record
+    def set_duration(self, duration):
+        self.duration = duration
+
+    def get_Switch(self):
+        return self.Switch
+
+    def set_Switch(self, Switch):
+        self.Switch = Switch
+        self._Switch_callback(self.Switch)
+        self.blocks_multiply_const_vxx_0.set_k(self.Switch)
 
 
 
