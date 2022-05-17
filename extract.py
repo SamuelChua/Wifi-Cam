78fbacc4f40065068ee1f5df11070eccf8d920fa
Samuel Chua <chua.jiacong.samuel@dhs.sg>
2022-05-17 11:30:22 +0800

    Customised with new_main function to open gnuradio locally

--- extract.py
+++ extract.py
@@ -8,6 +8,10 @@
 # Title: Extract
 # GNU Radio version: v3.8.2.0-57-gd71cd177
 
+
+
+# GNU Radio Generated Script with edits from line 257
+
 from distutils.version import StrictVersion
 
 if __name__ == '__main__':
@@ -76,11 +80,13 @@ class extract(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.prefix_0 = prefix_0 = 'C:\\Users\\intern\\Documents\\GitHub\\Wifi-Cam\\signal_data\\f'
         self.prefix = prefix = 'C:\\Users\\intern\\Documents\\GitHub\\Wifi-Cam\\signal_data\\filename'
         self.tuning = tuning = 2.422e9
+        self.time_0 = time_0 = prefix_0 + datetime.now().strftime("%H.%M.%S")
         self.time = time = prefix + datetime.now().strftime("%H.%M.%S")
-        self.samp_rate = samp_rate = 1e6
-        self.duration = duration = 1
+        self.samp_rate = samp_rate = 4e5
+        self.duration = duration = 5
         self.Switch = Switch = 0
 
         ##################################################
@@ -114,8 +120,9 @@ class extract(gr.top_block, Qt.QWidget):
             ),
         )
         self.uhd_usrp_source_0.set_center_freq(tuning, 0)
-        self.uhd_usrp_source_0.set_gain(50, 0)
-        self.uhd_usrp_source_0.set_antenna('TX/RX', 0)
+        self.uhd_usrp_source_0.set_rx_agc(False, 0)
+        self.uhd_usrp_source_0.set_gain(70, 0)
+        self.uhd_usrp_source_0.set_antenna('RX2', 0)
         self.uhd_usrp_source_0.set_bandwidth(samp_rate, 0)
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec())
@@ -137,10 +144,9 @@ class extract(gr.top_block, Qt.QWidget):
 
         self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(Switch)
-        self.blocks_message_debug_0 = blocks.message_debug()
         self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, time, False)
         self.blocks_file_sink_0.set_unbuffered(False)
-        self.analog_pwr_squelch_xx_0 = analog.pwr_squelch_cc(-140, 1e-4, 0, True)
+        self.analog_pwr_squelch_xx_0 = analog.pwr_squelch_cc(-140, 1e-4, 0, False)
 
 
 
@@ -158,6 +164,13 @@ class extract(gr.top_block, Qt.QWidget):
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
+    def get_prefix_0(self):
+        return self.prefix_0
+
+    def set_prefix_0(self, prefix_0):
+        self.prefix_0 = prefix_0
+        self.set_time_0(self.prefix_0 + datetime.now().strftime("%H.%M.%S"))
+
     def get_prefix(self):
         return self.prefix
 
@@ -172,6 +185,12 @@ class extract(gr.top_block, Qt.QWidget):
         self.tuning = tuning
         self.qtgui_sink_x_0.set_frequency_range(self.tuning, self.samp_rate)
         self.uhd_usrp_source_0.set_center_freq(self.tuning, 0)
+
+    def get_time_0(self):
+        return self.time_0
+
+    def set_time_0(self, time_0):
+        self.time_0 = time_0
 
     def get_time(self):
         return self.time
@@ -215,6 +234,37 @@ def main(top_block_cls=extract, options=None):
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+#Added main_new to set up 'local' GNURadio program to access the switch variable to turn on/off USRP 
+def main_new(tb, qapp, options=None):
+
+    # if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+    #     style = gr.prefs().get_string('qtgui', 'style', 'raster')
+    #     Qt.QApplication.setGraphicsSystem(style)
+    # qapp = Qt.QApplication(sys.argv)
+
+    # tb = top_block_cls()
 
     tb.start()
 
