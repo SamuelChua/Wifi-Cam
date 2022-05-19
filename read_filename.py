16fd5d6749c43be80511cd29acb343478e3c58ac
Samuel Chua <chua.jiacong.samuel@dhs.sg>
2022-05-19 13:28:00 +0800

    Removed previous code segments, added descriptions of how the calculations are done

--- read_filename.py
+++ read_filename.py
@@ -13,10 +13,6 @@ import time
 import seaborn as sns
 import matplotlib.image as mpimg 
 
-
-#e = np.fromfile(open("C:\\Users\\intern\\Documents\\Github\\Wifi-Cam\\filename"), dtype = np.complex64)
-#print (e)
-
 def simpleBinRead(filename, numSamps=-1, in_dtype=np.float32, out_dtype=np.complex64, offset=0):
     '''
     Simple, single-file complex data reader. numSamps refers to the number of complex samples.
@@ -26,41 +22,33 @@ def simpleBinRead(filename, numSamps=-1, in_dtype=np.float32, out_dtype=np.compl
     return data
 
 d = simpleBinRead('C:\\Users\\intern\\Documents\\Github\\Wifi-Cam\\signal_data\\filename15.46.11') #filename16.13.30 nice graph
-#peaks, _ = find_peaks(d, height = 0)
 
-mean_readings = []
-abs_power = np.abs(d)
-#trash = np.argwhere(np.diff(np.argwhere(abs_power == 0).flatten())) #array of index of all zeros
-#ii = np.argwhere(np.diff(ii.flatten()) != 1) #array of index of zeros but within the "on" recording
+mean_readings = [] #Creating a list to store mean power readings
+abs_power = np.abs(d) #Calculating absolute power 
+
 trash = np.argwhere(abs_power == 0) #indices of all zeros 
 ii = np.argwhere(abs_power != 0) #indices of all non_zeros
-gap = np.where(np.diff(ii.flatten()) > 10000) #last index of n-1 readings recorded
-#index = [0, -1]
-#new_gap = np.delete(gap, index)
-#print (new_gap) 
-gap1 = gap[0][:-1]
-print (gap1)
+gap = np.where(np.diff(ii.flatten()) > 10000) #out of all the non_zero data, returns the last index of every reading except the last reading
+gap1 = gap[0][:-1] #Remove last element from gap
+print (gap1) 
 
-#calculate mean reading for specfic horizontal and vertical angle (eg 30degree H and 30degrees V)
-mean_readings.append(np.mean(abs_power[ii.flatten()[1]:ii.flatten()[gap[0][0]]])) #first reading
+#calculate mean readings for specfic horizontal and vertical angle (eg 30degree Hori and 30degrees Vert)
+
+mean_readings.append(np.mean(abs_power[ii.flatten()[0]:ii.flatten()[gap[0][0]]])) #calculate mean of first reading
+#ii.flatten()[gap[0][0]] gives last index of first reading
+
 for i, elem in enumerate(gap1):  
-
-    if elem == gap1[-1]: #2nd last reading
-        mean_readings.append(np.mean(abs_power[ii.flatten()[elem+1]:ii.flatten()[gap[0][-1]]])) #2nd last reading
+    if elem == gap1[-1]: #2nd last reading of all the readings
+        mean_readings.append(np.mean(abs_power[ii.flatten()[elem+1]:ii.flatten()[gap[0][-1]]])) #ii.flatten()[elem] returns last index of 3rd last reading and +1 will return the first index of 2nd last reading. ii.flatten()[gap[0][-1]] returns last index of 2nd last reading
         break
     else:
-        mean_readings.append(np.mean(abs_power[ii.flatten()[elem+1]:ii.flatten()[gap1[i+1]]]))           
+        mean_readings.append(np.mean(abs_power[ii.flatten()[elem+1]:ii.flatten()[gap1[i+1]]])) #ii.flatten()[elem] returns last index of xth reading and +1 will return the first index of (x+1)th reading. ii.flatten()[gap1[i]] returns the index of the current reading in gap1 then +1 will return the index of the next reading in gap1 which are indices for abs_power
 
-mean_readings.append(np.mean(abs_power[ii.flatten()[gap[0][-1] + 1]:ii.flatten()[-1]])) #last reading
-
-
-
-
-
+mean_readings.append(np.mean(abs_power[ii.flatten()[gap[0][-1] + 1]:ii.flatten()[-1]])) #last reading: ii.flatten()[gap[0][-1]] returns last index of 2nd last reading and the +1 will return first index of last reading, ii.flatten()[-1] returns last non-zero index
 
 print ("Number of Readings:" + str(len(mean_readings)))  #all vertical/horizontal readings in one list 
 print (mean_readings)
-arr = np.array(mean_readings, dtype = np.float32) 
+arr = np.array(mean_readings, dtype = np.float32) #convert list to np.array
 
 interval()
 num_vert = len(interval.vert_interval) #number of vertical readings taken
@@ -69,67 +57,14 @@ num_hori = int(len(mean_readings)/num_vert)
 
 # arr = arr.reshape((num_vert, num_hori)).astype('float32') #need to double check how all the readings are changed in terms of order 
 arr = arr.reshape((num_hori, num_vert)).T
-arr = arr[np.arange(5,-1,-1),:]
+arr = arr[np.arange(5,-1,-1),:] #rearrange 2d array to desired format for picture display
 print (arr)
 print (arr.shape)
 x_axis_labels = interval.hori_interval # labels for x-axis
 y_axis_labels = np.flip(interval.vert_interval) # labels for y-axis
 ax = sns.heatmap(arr, annot = True, alpha = 0.5, zorder = 2, xticklabels=x_axis_labels, yticklabels=y_axis_labels)
 ax.set(xlabel="Horizontal Angle", ylabel="Vertical Angle")
-my_img = mpimg.imread("C:\\Users\\intern\\Downloads\\DSO.jpg")
+my_img = mpimg.imread("C:\\Users\\intern\\Downloads\\DSO.jpg") #Picture from webcam
 ax.imshow(my_img, aspect=ax.get_aspect(), extent=ax.get_xlim() + ax.get_ylim(), zorder=1)
 plt.show()
 
-# Make into PIL Image and save
-# img = Image.fromarray(arr)
-# if img.mode != 'RGB':
-#     img = img.convert('RGB')
-# img.save('new.png')
-# img.show()
-
-# pos_power = np.where(abs_power > 0)
-
-# power_db = 10*np.log10(power)
-# mean = np.sqrt(np.mean(power**2))
-# mean_db = 20*np.log10(mean)
-
-#plt.plot(abs_power)
-#plt.plot(peaks, d[peaks], "d")
-#plt.show()
-#plt.plot(power)
-#plt.show()
-
-#plt.plot(pos_power)
-#print (d[0:20000000].shape)
-#d = d[0:50000000].reshape(5000,10000)
-#print (d.shape)
-#plt.plot(np.mean(np.abs(d)))
-#plt.imshow
-#plt.show()
-
-
-
-# arduino=serial.Serial('COM3', 9600, timeout = .1)
-# time.sleep(2)
-
-
-# while 1:
-#     command = input("Servo Position: ")
-#     arduino.write(command.encode())
-
-
-# Define width and height
-#w, h = 50, 100
-
-# Read file using numpy "fromfile()"
-#with open('C:\\Users\\intern\\Documents\\Github\\Wifi-Cam\\signal_data\\filename', mode='rb') as f:
-    #d = np.fromfile(f,dtype=np.complex64,count=w*h).reshape(h,w)
-
-
-
-
-
-#img_rgb = cv2.cvtColor(d, cv2.COLOR_GRAY2BGR)
-#print (img_rgb.shape)
-#rgb = Image.fromarray(img_rgb)
-#rgb.save('rgb.png')
