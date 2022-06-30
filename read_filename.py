7fe2a6fa4d49f098ebdfee37f0c65d330c5efc68
Samuel Chua <chua.jiacong.samuel@dhs.sg>
2022-06-30 16:59:12 +0800

    Added array of images to add to final image instead of pano

--- read_filename.py
+++ read_filename.py
@@ -4,6 +4,7 @@ sys.path.append('C:\\Users\\intern\\Documents\\GitHub\\Wifi-Cam\\master')
 from master import interval
 from os import listdir
 from os.path import isfile, join
+import os
 from PIL import Image
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
@@ -21,7 +22,7 @@ def simpleBinRead(filename, numSamps=-1, in_dtype=np.float32, out_dtype=np.compl
     
     return data
 
-d = simpleBinRead('C:\\Users\\intern\\Documents\\Github\\Wifi-Cam\\signal_data\\filename15.46.11') #filename16.13.30 nice graph
+d = simpleBinRead('C:\\Users\\intern\\Documents\\Github\\Wifi-Cam\\signal_data\\filename16.51.43') #filename16.13.30 nice graph
 
 mean_readings = [] #Creating a list to store mean power readings
 abs_power = np.abs(d) #Calculating absolute power 
@@ -50,6 +51,23 @@ print ("Number of Readings:" + str(len(mean_readings)))  #all vertical/horizonta
 print (mean_readings)
 arr = np.array(mean_readings, dtype = np.float32) #convert list to np.array
 
+
+#Cropping Image
+num = 0
+folder = 'C:\\Users\\intern\\Documents\\Github\\Wifi-Cam\\image_data\\'
+
+myFolders = [file for file in os.listdir(folder) if file.endswith('.png')]
+myFolders.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+
+for f in myFolders:
+  path1 = os.path.join(folder,f)
+  im = Image.open(path1)
+  im_crop = im.crop((0, 0, 380, 480)) 
+  #im_crop = im.crop((210,0,380,320)) #center crop 
+  crop_dir = folder + '\\' + 'cropped' + '\\' + str(num) + '_cropped' + '.png'
+  im_crop.save(crop_dir, quality=95)
+  num += 1
+
 interval()
 num_vert = len(interval.vert_interval) #number of vertical readings taken
 print (num_vert)
@@ -62,9 +80,75 @@ print (arr)
 print (arr.shape)
 x_axis_labels = interval.hori_interval # labels for x-axis
 y_axis_labels = np.flip(interval.vert_interval) # labels for y-axis
+
+# Plot image + heatmap
 ax = sns.heatmap(arr, annot = True, alpha = 0.5, zorder = 2, xticklabels=x_axis_labels, yticklabels=y_axis_labels)
 ax.set(xlabel="Horizontal Angle", ylabel="Vertical Angle")
-my_img = mpimg.imread("C:\\Users\\intern\\Downloads\\DSO.jpg") #Picture from webcam
-ax.imshow(my_img, aspect=ax.get_aspect(), extent=ax.get_xlim() + ax.get_ylim(), zorder=1)
+
+
+vert_list = list(range(0,num_vert+1))
+hori_list = list(range(0,num_hori+1))
+reversed_vert_list = reversed(vert_list)
+
+#ax.imshow(my_img, aspect=ax.get_aspect(), extent=ax.get_xlim() + ax.get_ylim(), zorder=1)
+
+myFolders = [file for file in os.listdir(os.path.join(folder,'standby')) if file.endswith('.png')]
+myFolders.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+print (myFolders)
+
+
+index = num_vert
+index1 = num_vert
+index2 = num_vert
+index3 = num_vert
+index4 = num_vert
+
+def vindex_iter():
+    v_index = iter(reversed_vert_list)
+    for i in v_index:
+        v = i
+        print (v)
+        break
+    return v
+def hindex_iter(): 
+    h_index = iter(hori_list)
+    h = next(h_index)
+    print (h)
+    return h
+#Iter got issues 
+vindex_iter()
+hindex_iter()
+vindex_iter()
+hindex_iter()
+vindex_iter()
+hindex_iter()
+vindex_iter()
+hindex_iter()
+
+# TODO change 1,2,3,4,5 to hori_list
+for i in range(len(myFolders)):
+    img = os.path.join(r'C:\Users\intern\Documents\GitHub\Wifi-Cam\image_data\cropped', myFolders[i])
+    if i < num_vert:
+        my_img = mpimg.imread(img)
+        ax.imshow(my_img, aspect=ax.get_aspect(), extent=(0,1,vert_list[index],vert_list[index-1]), zorder=1)
+        index -= 1
+    elif i < 2*num_vert:
+        my_img = mpimg.imread(img)
+        ax.imshow(my_img, aspect=ax.get_aspect(), extent=(1,2,vert_list[index1],vert_list[index1-1]), zorder=1)
+        index1 -= 1
+    elif i < 3*num_vert:
+        my_img = mpimg.imread(img)
+        ax.imshow(my_img, aspect=ax.get_aspect(), extent=(2,3,vert_list[index2],vert_list[index2-1]), zorder=1)
+        index2 -= 1
+    elif i < 4*num_vert:
+        my_img = mpimg.imread(img)
+        ax.imshow(my_img, aspect=ax.get_aspect(), extent=(3,4,vert_list[index3],vert_list[index3-1]), zorder=1)
+        index3 -= 1
+    elif i < 5*num_vert:
+        my_img = mpimg.imread(img)
+        ax.imshow(my_img, aspect=ax.get_aspect(), extent=(4,5,vert_list[index4],vert_list[index4-1]), zorder=1)
+        index4 -= 1
+    else:
+        pass
 plt.show()
 
